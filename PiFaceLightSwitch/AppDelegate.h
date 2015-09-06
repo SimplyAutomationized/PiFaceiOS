91a10afd51434d858ee0defbec92b6f1ef2de2b3
Nathan Rees <Nextabyte@gmail.com>
2015-09-05 23:43:15 -0700

    Initial Commit

---
+++ PiFaceLightSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PiFaceLightSwitch
+//
+//  Created by Rees, Nathan on 9/5/15.
+//  Copyright (c) 2015 Simply Automationized. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PiFaceLightSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PiFaceLightSwitch
+//
+//  Created by Rees, Nathan on 9/5/15.
+//  Copyright (c) 2015 Simply Automationized. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
